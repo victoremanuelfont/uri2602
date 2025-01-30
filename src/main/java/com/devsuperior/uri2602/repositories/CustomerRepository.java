aedd524c85aa9fe2e6b673a9b5d8fc0d299046e3
Victor Emanuel <victoremanuelfont3@gmail.com>
2025-01-30 15:48:51 -0300

    Busca por JPQL

--- src/main/java/com/devsuperior/uri2602/repositories/CustomerRepository.java
+++ src/main/java/com/devsuperior/uri2602/repositories/CustomerRepository.java
@@ -1,5 +1,6 @@
 package com.devsuperior.uri2602.repositories;
 
+import com.devsuperior.uri2602.dto.CustomerMinDTO;
 import com.devsuperior.uri2602.entities.Customer;
 import com.devsuperior.uri2602.projection.CustomerMinProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,6 +15,9 @@ public interface CustomerRepository extends JpaRepository<Customer,Long> {
             "WHERE UPPER(state) = UPPER(:state)")
     List<CustomerMinProjection> search1(String state);
 
-
+    @Query("SELECT new com.devsuperior.uri2602.dto.CustomerMinDTO(obj.name) " +
+            "FROM Customer obj " +
+            "WHERE UPPER(obj.state) = UPPER(:state)")
+    List<CustomerMinDTO> search2(String state);
 
 }
