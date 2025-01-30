595ab7f4e9aaf123f0bd833d771776c8f69080fe
Victor Emanuel <victoremanuelfont3@gmail.com>
2025-01-30 14:56:40 -0300

    Para que a busca apareça no console

--- src/main/java/com/devsuperior/uri2602/Uri2602Application.java
+++ src/main/java/com/devsuperior/uri2602/Uri2602Application.java
@@ -1,12 +1,32 @@
 package com.devsuperior.uri2602;
 
+import com.devsuperior.uri2602.projection.CustomerMinProjection;
+import com.devsuperior.uri2602.repositories.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
-public class Uri2602Application {
+public class Uri2602Application implements CommandLineRunner {
+
+
+	@Autowired
+	private CustomerRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Uri2602Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		List<CustomerMinProjection> list = repository.search1("RS");
+		for(CustomerMinProjection obj : list){
+			System.out.println(obj.getName());
+		}
+
 	}
 }
